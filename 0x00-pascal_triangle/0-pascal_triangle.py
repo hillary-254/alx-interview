b7cd7f338da23aae3e5c5d5869a19f16ed6456ee
hillary-254 <hillarykim001@gmail.com>
2024-01-03 17:40:12 +0300

    fixing code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,27 +2,19 @@
 from typing import List
 
 
-def pascal_triangle(n: int) -> List[list]:
+def pascal_triangle(n):
     '''
     Pascal triangle
     '''
     if n <= 0:
         return []
 
-    if n == 1:
-        return [[1]]
-
-    if n == 2:
-        return [[1], [1, 1]]
-
-    triangle = [[1], [1, 1]]
-
-    for i in range(2, n):
-        temp = [1, 1]
-        for j in range(0, len(triangle[-1])-1):
-            a = triangle[-1][j]
-            b = triangle[-1][j+1]
-            temp.insert(-1, a + b)
-        triangle.append(temp)
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
 
     return triangle
